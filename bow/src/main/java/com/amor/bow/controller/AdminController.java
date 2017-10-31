8550dcd539b75ac37ab2e354ee9f02153695d758
pickear <pickear@gmail.com>
2017-10-31 14:49:58 +0800

    重构加载配置文件方式

--- bow/src/main/java/com/amor/bow/controller/AdminController.java
+++ bow/src/main/java/com/amor/bow/controller/AdminController.java
@@ -10,13 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by dylan on 2017/10/19.
  */
 @RestController
-public class BowController {
-
-    @RequestMapping("/")
-    @ResponseBody
-    public String http(){
-        return "http";
-    }
+public class AdminController {
 
     @RequestMapping("/reloadProperties")
     @ResponseBody
--- bow/src/main/java/com/amor/bow/listener/PropertiesModifiedListener.java
+++ bow/src/main/java/com/amor/bow/listener/PropertiesModifiedListener.java
@@ -1,55 +1,23 @@
 package com.amor.bow.listener;
 
-import com.amor.bow.config.BowProxyProperties;
 import com.amor.bow.helper.SpringBeanHolder;
 import com.amor.bow.listener.event.PropertiesModifiedEvent;
+import com.amor.bow.repository.impl.UserLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
-import org.springframework.boot.bind.PropertiesConfigurationFactory;
-import org.springframework.boot.context.config.ConfigFileApplicationListener;
-import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
-import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
-import org.springframework.core.io.support.SpringFactoriesLoader;
-import org.springframework.validation.BindException;
-
-import java.util.List;
 
 /**
  * Created by dell on 2017/10/25.
  */
-public class PropertiesModifiedListener extends ConfigFileApplicationListener{
+public class PropertiesModifiedListener implements ApplicationListener<PropertiesModifiedEvent> {
 
     private final static Logger logger = LoggerFactory.getLogger(PropertiesModifiedListener.class);
 
-    private ApplicationEnvironmentPreparedEvent environmentPreparedEvent = null;
     @Override
-    public void onApplicationEvent(ApplicationEvent event) {
-        if(event instanceof ApplicationEnvironmentPreparedEvent){
-            this.environmentPreparedEvent = (ApplicationEnvironmentPreparedEvent)event;
-        }
-        if(event instanceof PropertiesModifiedEvent){
-            logger.info("listen a prpperties modified event,reload the properties.");
-            AutowireCapableBeanFactory beanFactory = SpringBeanHolder.getContext()
-                    .getAutowireCapableBeanFactory();
-
-            BowProxyProperties properties = SpringBeanHolder.getBean(BowProxyProperties.class);
-            if(null != properties){
-                beanFactory.destroyBean(properties);
-            }
-            postProcessEnvironment(environmentPreparedEvent.getEnvironment(),environmentPreparedEvent.getSpringApplication());
-            beanFactory.createBean(BowProxyProperties.class);
-            /*List<PropertiesConfigurationFactory> propertiesConfigurationFactories =  SpringFactoriesLoader.
-                    loadFactories(PropertiesConfigurationFactory.class,getClass().getClassLoader());
-            propertiesConfigurationFactories.forEach(factory ->{
-                try {
-                    factory.bindPropertiesToTarget();
-                } catch (BindException e) {
-                    e.printStackTrace();
-                }
-            });*/
-        }
-
+    public void onApplicationEvent(PropertiesModifiedEvent event) {
+        logger.info("listen properties modified event,reload it!");
+        UserLoader loader = SpringBeanHolder.getBean(UserLoader.class);
+        loader.reload();
     }
 }
